f70249ad3127034fee30ffce1b2ed22d7b0ab5bf
Aliocha Seror <aliomatou@gmail.com>
2024-11-12 14:26:56 +0100

    stack à taille choisie et stack infinie

--- Int Stack.cpp
+++ Int Stack.cpp
@@ -1,4 +1,4 @@
-#include <iostream>;
+#include <iostream>
 using namespace std;
 
 class IntStack10
@@ -9,7 +9,7 @@ private:
 
 public:
     void push(int n){
-        if ( last < 10 )
+        if ( last < 9 )
         {
             content[++last] = n;
         }
@@ -42,11 +42,72 @@ class IntStack
 {
 private:
     int size;
+    int last = -1;
     int* content;
 public:
+    
     void InitStack(int n){
         this -> size = n;
-        this -> content = new int[n];
+        this -> content = new int[n]; // Le new permet d'accepter une taille variable.
+    };
+
+    void push(int n){
+        if ( last < size - 1 )
+        {
+            content[++last] = n;
+        }
+        else
+        {
+            cout << "Stack is full.";
+        }
+    };
+    
+    int pop(){
+        if (last >= 0) {
+            return content[last--];
+        }
+        else {
+            cout << "Stack is empty";
+            return 0;
+        }
+    };
+    
+    void print(){
+        for (int i = 0; i <= last; i++)
+        {
+            cout << content[i] << " ";
+        }
+        cout << endl;;
+    }
+};
+
+class IntfiniteStack
+{
+private:
+    int last = -1;
+    int* content;
+public:
+
+    void push(int n){
+        content[++last] = n;
+    };
+    
+    int pop(){
+        if (last >= 0) {
+            return content[last--];
+        }
+        else {
+            cout << "Stack is empty";
+            return 0;
+        }
+    };
+    
+    void print(){
+        for (int i = 0; i <= last; i++)
+        {
+            cout << content[i] << " ";
+        }
+        cout << endl;;
     }
 };
 
@@ -63,4 +124,17 @@ int main() {
     cout << stack.pop() << endl;;
 
     stack.print();
+
+    IntStack stack2;
+    stack2.InitStack(2);
+    stack2.push(2);
+    stack2.push(5);
+    stack2.push(1);
+
+    stack2.print();
+
+    stack2.pop();
+    cout << stack2.pop() << endl;;
+
+    stack2.print();
 }
